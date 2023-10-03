8bf4dbce09cd94f51d96a082b550f63d0556b9bc
priyanshukr449 <priyanshukr449@gmail.com>
2023-10-03 13:35:41 +0530

    store and components are created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import authService from './appwrite/auth'
+import {login,logout} from './features/auth/authSlice'
+import {Header, Footer} from './components/index'
 
 
 function App() {
-  c
-  return (
-    <>
-      <h1 className="text-red-500">A blog app</h1>
-    </>
-  );
+  const [loading , setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout());
+      }
+    }).finally(() => {
+      setLoading(false);
+    })
+  },[])
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap bg-gray-800 content-between">
+      <div className="w-full block">
+        <Header />
+        <main>
+          {/* outlet */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App;
